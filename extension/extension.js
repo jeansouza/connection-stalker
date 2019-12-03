f9ba1882f6e1b52fa1c2ccb387a087fed0f14308
Jean Souza <jeancfsouza@gmail.com>
2019-12-02 22:20:49 -0300

    Improving code

--- extension/extension.js
+++ extension/extension.js
@@ -1,27 +1,34 @@
-const St = imports.gi.St
-const Main = imports.ui.main
-const Soup = imports.gi.Soup
-const Mainloop = imports.mainloop
-const Clutter = imports.gi.Clutter
-const PanelMenu = imports.ui.panelMenu
-const Gio = imports.gi.Gio
-const Shell = imports.gi.Shell
-const ExtensionUtils = imports.misc.extensionUtils
-const Me = ExtensionUtils.getCurrentExtension()
-const Metadata = Me.metadata
-const Convenience = Me.imports.convenience
-const Settings = Convenience.getSettings()
-const GLib = imports.gi.GLib
-const PopupMenu = imports.ui.popupMenu
-const CLIPBOARD_TYPE = St.ClipboardType.CLIPBOARD
-const Clipboard = St.Clipboard.get_default()
-const NO_CONNECTION = 'Waiting for connection'
+'use strict'
+
+// Imports
+const CLUTTER = imports.gi.Clutter
+const G_LIB = imports.gi.GLib
+const GIO = imports.gi.Gio
+const EXTENSION_UTILS = imports.misc.extensionUtils
+const MAIN = imports.ui.main
+const MAIN_LOOP = imports.mainloop
+const PANEL_MENU = imports.ui.panelMenu
+const POPUP_MENU = imports.ui.popupMenu
+const SHELL = imports.gi.Shell
+const SOUP = imports.gi.Soup
+const ST = imports.gi.St
+
+// Constants
 const CANT_GET_LOCAL_IP = `Can't get local ip`
-const MENU_POSITION = 'right'
+const ME = EXTENSION_UTILS.getCurrentExtension()
+const CONVENIENCE = ME.imports.convenience
 const CONNECTION_REFUSED = 'Connection refused'
+const CLIPBOARD = ST.Clipboard.get_default()
+const CLIPBOARD_TYPE = ST.ClipboardType.CLIPBOARD
+const ERROR_ICON = GIO.icon_new_for_string(`${ME.path}/icons/flags/error.png`)
+const MENU_POSITION = 'right'
+const METADATA = ME.metadata
+const NO_CONNECTION = 'Waiting for connection'
+const SETTINGS = CONVENIENCE.getSettings()
 const STATUS_AREA_ID = 'connection-stalker-status-area'
 
-const DEFAULT_DATA = {
+// Globals
+const _defaultData = {
   ip: 'no connection',
   hostname: '',
   city: '',
@@ -32,14 +39,14 @@ const DEFAULT_DATA = {
   postal: '',
   timezone: ''
 }
-
-let _label, _icon
+let _label = null
+let _icon = null
 let _ipInfoRows = {}
-let _ipInfoBox
+let _ipInfoBox = null
 
 const makeHttpSession = () => {
-  let httpSession = new Soup.SessionAsync()
-  Soup.Session.prototype.add_feature.call(httpSession, new Soup.ProxyResolverDefault())
+  const httpSession = new SOUP.SessionAsync()
+  SOUP.Session.prototype.add_feature.call(httpSession, new SOUP.ProxyResolverDefault())
   return httpSession
 }
 
@@ -48,10 +55,10 @@ const servicesRequestProcessors = {
     endpoint: 'https://ipinfo.io/',
     process: function (callback) {
       let httpSession = makeHttpSession()
-      let request = Soup.Message.new('GET', this.endpoint)
-      const _processRequest = (httpSession, message) => {
+      let request = SOUP.Message.new('GET', this.endpoint)
+      const processRequest = (httpSession, message) => {
         if (message.status_code !== 200) {
-          callback(message.status_code, null)
+          callback(null)
           return
         }
         let responseJSON = request.response_body.data
@@ -60,16 +67,16 @@ const servicesRequestProcessors = {
 
         clearIpInfoRows()
 
-        Object.keys(responseData).map(function(key) {
+        Object.keys(responseData).map(function (key) {
           try {
             setIpInfoRow(key, responseData[key])
-          } catch(e) {}
+          } catch (e) { }
         })
 
-        callback(null, simplifiedResponseData)
+        callback(simplifiedResponseData)
       }
 
-      httpSession.queue_message(request, _processRequest)
+      httpSession.queue_message(request, processRequest)
     }
   },
 
@@ -77,10 +84,10 @@ const servicesRequestProcessors = {
     endpoint: `http://ip-api.com/json/?fields=status,countryCode,query`,
     process: function (callback) {
       let httpSession = makeHttpSession()
-      let request = Soup.Message.new('GET', this.endpoint)
-      const _processRequest = (httpSession, message) => {
+      let request = SOUP.Message.new('GET', this.endpoint)
+      const processRequest = (httpSession, message) => {
         if (message.status_code !== 200) {
-          callback(message.status_code, null)
+          callback(null)
           return
         }
         let responseJSON = request.response_body.data
@@ -88,46 +95,15 @@ const servicesRequestProcessors = {
         let simplifiedResponseData = { ip: responseData.query, countryCode: responseData.countryCode }
         clearIpInfoRows()
 
-        Object.keys(responseData).map(function(key) {
+        Object.keys(responseData).map(function (key) {
           try {
             setIpInfoRow(key, responseData[key])
-          } catch(e) {}
+          } catch (e) { }
         })
-        callback(null, simplifiedResponseData)
+        callback(simplifiedResponseData)
       }
 
-      httpSession.queue_message(request, _processRequest)
-    }
-  },
-
-  'ipapi.co': {
-    endpoint: `https://ipapi.co/json/`,
-    process: function (callback) {
-      let httpSession = makeHttpSession()
-      let request = Soup.Message.new('GET', this.endpoint)
-      const _processRequest = (httpSession, message) => {
-        if (message.status_code !== 200) {
-          callback(message.status_code, null)
-          return
-        }
-        let responseJSON = request.response_body.data
-        let responseData = JSON.parse(responseJSON)
-        if (responseData.error) {
-          callback(responseData.reason, null)
-          return
-        }
-        let simplifiedResponseData = { ip: responseData.ip, countryCode: responseData.country }
-        clearIpInfoRows()
-
-        Object.keys(responseData).map(function(key) {
-          try {
-            setIpInfoRow(key, responseData[key])
-          } catch(e) {}
-        })
-        callback(null, simplifiedResponseData)
-      }
-
-      httpSession.queue_message(request, _processRequest)
+      httpSession.queue_message(request, processRequest)
     }
   },
 
@@ -135,10 +111,10 @@ const servicesRequestProcessors = {
     endpoint: `https://api.myip.com`,
     process: function (callback) {
       let httpSession = makeHttpSession()
-      let request = Soup.Message.new('GET', this.endpoint)
-      const _processRequest = (httpSession, message) => {
+      let request = SOUP.Message.new('GET', this.endpoint)
+      const processRequest = (httpSession, message) => {
         if (message.status_code !== 200) {
-          callback(message.status_code, null)
+          callback(null)
           return
         }
         let responseJSON = request.response_body.data
@@ -146,15 +122,15 @@ const servicesRequestProcessors = {
         let simplifiedResponseData = { ip: responseData.ip, countryCode: responseData.cc }
         clearIpInfoRows()
 
-        Object.keys(responseData).map(function(key) {
+        Object.keys(responseData).map(function (key) {
           try {
             setIpInfoRow(key, responseData[key])
-          } catch(e) {}
+          } catch (e) { }
         })
-        callback(null, simplifiedResponseData)
+        callback(simplifiedResponseData)
       }
 
-      httpSession.queue_message(request, _processRequest)
+      httpSession.queue_message(request, processRequest)
     }
   },
 
@@ -162,10 +138,10 @@ const servicesRequestProcessors = {
     endpoint: `https://api.ip.sb/geoip`,
     process: function (callback) {
       let httpSession = makeHttpSession()
-      let request = Soup.Message.new('GET', this.endpoint)
-      const _processRequest = (httpSession, message) => {
+      let request = SOUP.Message.new('GET', this.endpoint)
+      const processRequest = (httpSession, message) => {
         if (message.status_code !== 200) {
-          callback(message.status_code, null)
+          callback(null)
           return
         }
         let responseJSON = request.response_body.data
@@ -173,98 +149,107 @@ const servicesRequestProcessors = {
         let simplifiedResponseData = { ip: responseData.ip, countryCode: responseData.country_code }
         clearIpInfoRows()
 
-        Object.keys(responseData).map(function(key) {
+        Object.keys(responseData).map(function (key) {
           try {
             setIpInfoRow(key, responseData[key])
-          } catch(e) {}
+          } catch (e) { }
         })
-        callback(null, simplifiedResponseData)
+        callback(simplifiedResponseData)
       }
 
-      httpSession.queue_message(request, _processRequest)
+      httpSession.queue_message(request, processRequest)
     }
   },
   // thanks to https://github.com/Josholith/gnome-extension-lan-ip-address/blob/master/extension.js
   'local-ip': {
     process: function (callback) {
-      const commandOutputBytes = GLib.spawn_command_line_sync('ip route get 1.1.1.1')[1]
-      let commandOutputString = Array.from(commandOutputBytes).reduce(
-        (accumulator, currentValue) => accumulator + String.fromCharCode(currentValue),
-        ''
-      )
-      let matches = commandOutputString.match(/src [^ ]+/g)
-      const lanIpAddress = matches ? matches[0].split(' ')[1] : CANT_GET_LOCAL_IP
-      clearIpInfoRows()
+      let lanIpAddress = CANT_GET_LOCAL_IP
 
-        Object.keys(responseData).map(function(key) {
+      try {
+        const commandOutputBytes = G_LIB.spawn_command_line_sync('ip route get 1.1.1.1')[1]
+        let commandOutputString = Array.from(commandOutputBytes).reduce(
+          (accumulator, currentValue) => accumulator + String.fromCharCode(currentValue),
+          ''
+        )
+        let matches = commandOutputString.match(/src [^ ]+/g)
+        lanIpAddress = matches ? matches[0].split(' ')[1] : CANT_GET_LOCAL_IP
+
+        clearIpInfoRows()
+
+        Object.keys(responseData).map(function (key) {
           try {
             setIpInfoRow(key, responseData[key])
-          } catch(e) {}
+          } catch (e) { }
         })
-      callback(null, { ip: lanIpAddress })
+      } catch (e) { }
+
+      callback({ ip: lanIpAddress, countryCode: 'LOCAL' })
     }
   }
 }
 
 const displayModeProcessors = {
-  'ip-flag-and-country': (err, responseData) => {
-    _label.text = !responseData ? CONNECTION_REFUSED : `${responseData.countryCode} | ${responseData.ip}`
-
-    _icon.gicon = !responseData ?
-      Gio.icon_new_for_string(`${Me.path}/icons/flags/error.png`) :
-      selectIcon(responseData)
+  'IP, flag and country': (responseData) => {
+    if (!responseData) {
+      _label.text = CONNECTION_REFUSED
+      _icon.gicon = ERROR_ICON
+      _icon.style_class = 'system-status-icon'
+    } else {
+      _label.text = `${responseData.countryCode} | ${responseData.ip}`
+      _icon.gicon = GIO.icon_new_for_string(`${ME.path}/icons/flags/${responseData.countryCode}.png`)
+      _icon.style_class = 'icon-style-custom'
+    }
   },
-  'IP and flag': (err, responseData) => {
-    _label.text = !responseData ? CONNECTION_REFUSED : responseData.ip
-
-    _icon.gicon = !responseData ?
-      Gio.icon_new_for_string(`${Me.path}/icons/flags/error.png`) :
-      selectIcon(responseData)
-
-    _icon.style_class = !responseData ? 'system-status-icon' : _icon.style_class
+  'IP and flag': (responseData) => {
+    if (!responseData) {
+      _label.text = CONNECTION_REFUSED
+      _icon.gicon = ERROR_ICON
+      _icon.style_class = 'system-status-icon'
+    } else {
+      _label.text = responseData.ip
+      _icon.gicon = GIO.icon_new_for_string(`${ME.path}/icons/flags/${responseData.countryCode}.png`)
+      _icon.style_class = 'icon-style-custom'
+    }
   },
-  'only-flag': (err, responseData) => {
+  'Only flag': (responseData) => {
     _label.text = ''
 
-    _icon.gicon = !responseData ?
-      Gio.icon_new_for_string(`${Me.path}/icons/flags/error.png`) :
-      selectIcon(responseData)
+    if (!responseData) {
+      _icon.gicon = ERROR_ICON
+      _icon.style_class = 'system-status-icon'
+    } else {
+      _icon.gicon = GIO.icon_new_for_string(`${ME.path}/icons/flags/${responseData.countryCode}.png`)
+      _icon.style_class = 'custom-icon-style-only-flag'
+    }
   },
-  'only-ip': (err, responseData) => {
-    _label.text = !responseData ? CONNECTION_REFUSED : responseData.ip
-
+  'Only IP': (responseData) => {
     _icon.gicon = null
+    _icon.style_class = ''
+    _label.text = !responseData ? CONNECTION_REFUSED : responseData.ip
   }
 }
 
-const selectIcon = (responseData) => {
-  const currentService = Settings.get_string('api-service')
-  return currentService === 'local-ip' ?
-    Gio.icon_new_for_string(`${Me.path}/icons/flags/local-ip-icon.png`) :
-    Gio.icon_new_for_string(`${Me.path}/icons/flags/${responseData.countryCode}.png`)
-}
-
 const _makeRequest = () => {
-  const currentService = Settings.get_string('api-service'),
-    currentMode = Settings.get_string('display-mode')
+  const currentService = SETTINGS.get_string('api-service')
+  const currentMode = SETTINGS.get_string('display-mode')
   const service = servicesRequestProcessors[currentService]
   const requestCallback = displayModeProcessors[currentMode]
   service.process(requestCallback)
 }
 
-class IpInfoIndicator extends PanelMenu.Button {
+class IpInfoIndicator extends PANEL_MENU.Button {
   constructor() {
-    super(0.0, "Ip Info Indicator", false)
-    let hbox = new St.BoxLayout({ style_class: 'ip-data-panel' })
+    super(0.0, 'Ip Info Indicator', false)
+    let hbox = new ST.BoxLayout({ style_class: 'ip-data-panel' })
 
-    _icon = new St.Icon({
+    _icon = new ST.Icon({
       gicon: null,
-      style_class: 'custom-icon-style'
+      style_class: 'icon-style-custom'
     })
 
-    _label = new St.Label({
-      text: Settings.get_boolean('display-mode') ? '' : NO_CONNECTION,
-      y_align: Clutter.ActorAlign.CENTER
+    _label = new ST.Label({
+      text: SETTINGS.get_boolean('display-mode') ? '' : NO_CONNECTION,
+      y_align: CLUTTER.ActorAlign.CENTER
     })
 
     hbox.add_child(_icon)
@@ -273,40 +258,37 @@ class IpInfoIndicator extends PanelMenu.Button {
     this.actor.add_actor(hbox)
 
     //main containers
-    let ipInfo = new PopupMenu.PopupBaseMenuItem({reactive: false})
-    let parentContainer = new St.BoxLayout() //main container that holds ip info and map
-    //
+    let ipInfo = new POPUP_MENU.PopupBaseMenuItem({ reactive: false })
+    let parentContainer = new ST.BoxLayout() //main container that holds ip info and map
 
     //ipinfo
-    _ipInfoBox = new St.BoxLayout({style_class: 'ip-info-box', vertical: true})
+    _ipInfoBox = new ST.BoxLayout({ style_class: 'ip-info-box', vertical: true })
     parentContainer.add_actor(_ipInfoBox)
     ipInfo.actor.add(parentContainer)
     this.menu.addMenuItem(ipInfo)
 
-    Object.keys(DEFAULT_DATA).map((key) => {
-      setIpInfoRow(key, DEFAULT_DATA[key])
+    Object.keys(_defaultData).map((key) => {
+      setIpInfoRow(key, _defaultData[key])
     })
 
-    let _appSys = Shell.AppSystem.get_default()
-    let _gsmPrefs = _appSys.lookup_app('gnome-shell-extension-prefs.desktop')
+    const appSys = SHELL.AppSystem.get_default()
+    const gsmPrefs = appSys.lookup_app('gnome-shell-extension-prefs.desktop')
 
-    let prefs
+    const prefs = new POPUP_MENU.PopupMenuItem('Preferences...')
 
-    prefs = new PopupMenu.PopupMenuItem("Preferences...")
-
-    prefs.connect('activate', function() {
-      if (_gsmPrefs.get_state() == _gsmPrefs.SHELL_APP_STATE_RUNNING) {
-        _gsmPrefs.activate()
+    prefs.connect('activate', () => {
+      if (gsmPrefs.get_state() === gsmPrefs.SHELL_APP_STATE_RUNNING) {
+        gsmPrefs.activate()
       } else {
-        let info = _gsmPrefs.get_app_info()
-        let timestamp = global.display.get_current_time_roundtrip()
-        info.launch_uris([Metadata.uuid], global.create_app_launch_context(timestamp, -1))
+        const info = gsmPrefs.get_app_info()
+        const timestamp = global.display.get_current_time_roundtrip()
+        info.launch_uris([METADATA.uuid], global.create_app_launch_context(timestamp, -1))
       }
     })
 
     this.menu.addMenuItem(prefs)
 
-    Main.panel.addToStatusArea(STATUS_AREA_ID, this, 1, MENU_POSITION)
+    MAIN.panel.addToStatusArea(STATUS_AREA_ID, this, 1, MENU_POSITION)
 
     this.destroy = () => {
       this.removeTimer()
@@ -320,20 +302,20 @@ class IpInfoIndicator extends PanelMenu.Button {
 
     this.removeTimer = () => {
       if (this.timer) {
-        Mainloop.source_remove(this.timer)
+        MAIN_LOOP.source_remove(this.timer)
         this.timer = null
       }
     }
 
     this.updateRefreshRate = () => {
-      this.refreshRate = Settings.get_int('refresh-rate')
+      this.refreshRate = SETTINGS.get_int('refresh-rate')
       this.removeTimer()
-      this.timer = Mainloop.timeout_add_seconds(this.refreshRate, this.update.bind(this))
+      this.timer = MAIN_LOOP.timeout_add_seconds(this.refreshRate, this.update.bind(this))
     }
 
     this.updateDisplayMode = () => {
-      Main.panel.statusArea[STATUS_AREA_ID] = null
-      Main.panel.addToStatusArea(STATUS_AREA_ID, this, 1, MENU_POSITION)
+      MAIN.panel.statusArea[STATUS_AREA_ID] = null
+      MAIN.panel.addToStatusArea(STATUS_AREA_ID, this, 1, MENU_POSITION)
       this.update()
     }
 
@@ -345,9 +327,9 @@ class IpInfoIndicator extends PanelMenu.Button {
       this.update()
     }
 
-    Settings.connect('changed::refresh-rate', this.updateRefreshRate.bind(this))
-    Settings.connect('changed::display-mode', this.updateDisplayMode.bind(this))
-    Settings.connect('changed::api-service', this.updateService.bind(this))
+    SETTINGS.connect('changed::refresh-rate', this.updateRefreshRate.bind(this))
+    SETTINGS.connect('changed::display-mode', this.updateDisplayMode.bind(this))
+    SETTINGS.connect('changed::api-service', this.updateService.bind(this))
 
     this.actor.connect('button-press-event', this.onClick.bind(this))
 
@@ -373,22 +355,22 @@ const clearIpInfoRows = () => {
 const setIpInfoRow = (key, value) => {
   removeItemFromIpInfoRows(key)
 
-  const ipInfoRow = new St.BoxLayout()
+  const ipInfoRow = new ST.BoxLayout()
 
-  ipInfoRow.add_actor(new St.Label({
+  ipInfoRow.add_actor(new ST.Label({
     style_class: 'ip-info-key',
     text: key + ': '
   }))
 
-  const dataLabelBtn = new St.Button({
-    child: new St.Label({
+  const dataLabelBtn = new ST.Button({
+    child: new ST.Label({
       style_class: 'ip-info-value',
       text: value
     })
   })
 
-  dataLabelBtn.connect('button-press-event', function() {
-    Clipboard.set_text(CLIPBOARD_TYPE, dataLabelBtn.child.text)
+  dataLabelBtn.connect('button-press-event', function () {
+    CLIPBOARD.set_text(CLIPBOARD_TYPE, dataLabelBtn.child.text)
   })
 
   ipInfoRow.add_actor(dataLabelBtn)
